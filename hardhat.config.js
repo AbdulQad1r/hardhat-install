d52829b7a722c1a4dd59f9d126d3b76ecc16b5c0
Abdul Qadir <abdultheqadir@gmail.com>
2022-11-26 20:01:05 +0500

    hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "yNa717og6hMFm0l1vB8grzPxOcyICuWv";
+const ALCHEMY_API_KEY = "Alchemy Key";
 
 // Replace this private key with your Goerli account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY =
-  "e1c45d4e7f2bf445e087ccb4c28c3cbb3084fe2e128da926fb2119c40bdad865";
+const GOERLI_PRIVATE_KEY = "Private Key";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
